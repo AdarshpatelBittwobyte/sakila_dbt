11a380641da2c3ca8eaf866142692e3dfc75330f
AdarshpatelBittwobyte <adarsh.patel@bittwobyte.com>
2024-04-02 15:53:57 +0530

    new

--- adventure/macros/sales_order_years.sql
+++ adventure/macros/sales_order_years.sql
@@ -6,4 +6,26 @@
     INNER JOIN   {{source('adventure','salesorderheader')}} s2 
     ON s.sales_order_id = s2.sales_order_id 
     WHERE EXTRACT(YEAR FROM ORDER_DATE) = '{{ year }}'
-{% endmacro %}
+{% endmacro %} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ {% if dt | length > 10 %}
+        left({{ dt }}, 10)
+    {% else %}
+        {{ dt }}
+    {% endif %}
--- adventure/models/dbt_projects/fact_sales.sql
+++ adventure/models/dbt_projects/fact_sales.sql
@@ -1,14 +1,13 @@
 
 WITH cte_sales_order AS (
- -- {%- set years = run_query("SELECT DISTINCT EXTRACT(YEAR FROM modified_date)::INT AS year FROM stg.salesorderdetail ") %}
+ {%- set years = run_query("SELECT DISTINCT EXTRACT(YEAR FROM modified_date)::INT AS year FROM stg.salesorderdetail ") -%}
 
-{%- set years = ['2011', '2012', '2013', '2014'] -%}
+--{%- set years = ['2011', '2012', '2013', '2014'] -%}
     
 {%- for year in years -%}
-    {{ config(materialized='incremental', unique_key=['soh.sales_order_id', 'sales_order_detail_id'], alias='fact_sales_order') }}
+    {{ config(materialized='incremental', unique_key=['sales_order_id', 'sales_order_detail_id'], alias='fact_sales_order') }}
 
         select *
-        --,cast('01/01/1999' as date)::timestamp as etl_time
         from dwh.sales_order_year_{{ year }} 
         where 1=1
          {% if is_incremental() %}
@@ -27,12 +26,12 @@ salesorderheaders AS (
 final AS (
     SELECT
         soh.sales_order_id,
-        sod.sales_order_detail_id,
         sod.order_qty,
         sod.unit_price,
         soh.tax_amt,
         (sod.order_qty * sod.unit_price) AS gross_sale,
-        (sod.order_qty * sod.unit_price - soh.tax_amt) AS net_sale
+        (sod.order_qty * sod.unit_price - soh.tax_amt) AS net_sale,
+        cast('01/01/1999' as date)::timestamp as etl_time
     FROM  
         cte_sales_order sod
     INNER JOIN 
