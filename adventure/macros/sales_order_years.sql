cfbb98602312433829dc993e1c81fe2c5fa37c6a
AdarshpatelBittwobyte <adarsh.patel@bittwobyte.com>
2024-04-29 18:29:41 +0530

    new

--- adventure/macros/sales_order_years.sql
+++ adventure/macros/sales_order_years.sql
@@ -7,3 +7,18 @@
     ON s.sales_order_id = s2.sales_order_id 
     WHERE EXTRACT(YEAR FROM ORDER_DATE) = '{{ year }}'
 {% endmacro %} 
+/*
+{% macro sales_order_year(year) %}
+    {% set table_alias = 'sales_order_year_' ~ year %}
+    {{ config(alias=table_alias) }}
+    CREATE TABLE dwh.sales_order_year_{{ year }} AS (
+        SELECT s.*, s2.sub_total, s2.total_due, EXTRACT(YEAR FROM Order_date) AS "Year",
+        CAST('01/01/1999' AS DATE)::TIMESTAMP AS etl_time
+       FROM {{source('adventure','salesorderdetail')}} s
+    INNER JOIN   {{source('adventure','salesorderheader')}} s2 
+        ON s.sales_order_id = s2.sales_order_id 
+        WHERE EXTRACT(YEAR FROM ORDER_DATE) = '{{ year }}'
+    );
+
+    SELECT * FROM {{ table_alias }};
+{% endmacro %}  */
--- adventure/models/dbt_projects/sales_order_year.sql
+++ adventure/models/dbt_projects/sales_order_year.sql
@@ -1,5 +1,12 @@
-
+/*
 {{
   config(materialized='table')
  
 }} {{ sales_order_years(2014) }}
+*/
+{%- set years = run_query("SELECT DISTINCT EXTRACT(YEAR FROM modified_date)::INT AS year FROM stg.salesorderdetail ") -%}
+
+{% for year_data in years %}
+    {% set year = year_data.year %}
+    {{ sales_order_year(year) }}
+{% endfor %}
