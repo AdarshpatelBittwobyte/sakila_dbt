b9244c02201711091f447f24e77ba5b501b0ef67
AdarshpatelBittwobyte <adarsh.patel@bittwobyte.com>
2024-05-02 14:02:30 +0530

    welcome

--- dlt/test.py
+++ dlt/test.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, text
 from sql_database import sql_database
 from sqlalchemy import text
 
-def load_database_table(credential, db_name, db_schema, incremental_by):
+def load_database_table( db_name,db_schema,credential,incremental_by):
     try:
         engine = create_engine(credential)  # Assuming credential is a connection string
         source = sql_database(credentials=credential, schema=db_schema)
@@ -22,7 +22,7 @@ def load_database_table(credential, db_name, db_schema, incremental_by):
                 pipeline_name=db_name, destination="postgres", dataset_name="loaded_data"
             )
             info = pipeline.run(source.with_resources(table_name), table_name=f"{table_name}_{db_name}", write_disposition="merge")
-        
+            print(f"Number of rows in {table_name} after loading: {row_count}")
     except Exception as e:
         print(f"Error loading data for {db_name}: {e}")
 if __name__ == "__main__":
@@ -36,6 +36,6 @@ if __name__ == "__main__":
         for row in rows:
             db_name, db_schema, credential, incremental_by = row
             print(row)
-            load_database_table(credential, db_name, db_schema, incremental_by)
+            load_database_table(db_name,db_schema,credential,incremental_by)
     except Exception as e:
         print(f"Error in main script: {e}")
